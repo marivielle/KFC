7051955b65f44961d7a5d452ef795480a96324f5
Mariana <marivielle@gmail.com>
2018-10-16 14:01:27 +0100

    Item class

---
+++ item.py
@@ -0,0 +1,6 @@
+class Item():
+    def __init__(self, name, description, ID):
+        self.name = name
+        self.reduce_inv = False
+        self.description = description
+        self.id = ID
