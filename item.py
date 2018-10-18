b4d1a58b4b90ff90ccc98cb9e0de737811ca68eb
Mariana Capdevielle Schurmann <marivielle@gmail.com>
2018-10-18 15:49:37 +0100

    Update item.py

--- item.py
+++ item.py
@@ -1,5 +1,5 @@
 class item():
-    def __init__(self, name, description, identification, strength):
+    def __init__(self, name, description = "", identification, strength = 0):
         self.name = name
         self.reduce_inv = False #whether the item is large enough to reduce inventory space
         self.description = description #item description
