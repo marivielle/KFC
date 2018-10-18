a92cc7e09b3931fe7627e1708be3749ad0de3fb5
koraberisha <43994080+koraberisha@users.noreply.github.com>
2018-10-18 16:13:33 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -11,11 +11,40 @@ class game():
 
 
     def generate_rooms_items(self):
-        self.rooms.append(room("Courtyard",[item("Rock","rk1"),item("ID Card","id1"),item("Large Log","lg1")]))
-        self.rooms.append(room("Reception",[item("Screwdriver","sr1"),item("Flashlight","ft1")]))
-        self.rooms.append(room("Toilet",[item("Toilet Brush","th1"),item("Bandage","be1"),item("Toothpaste","te1")]))
-        self.rooms.append(room("Storage Closet",[item("Mop","mp1"),item("Drain Unblocker","dr1"),item("Newspaper","nr1")]))
-        self.rooms.append(room("Hallway",[item("Sunflower Kirill","sl1")]))
+        self.rooms.append(room("Courtyard",
+                               [item("Rock","rk1"),
+                                item("ID Card","id1"),
+                                item("Large Log","lg1")],
+                               ["Reception"],
+                               {"Reception":["Front door","Window"]}))
+        
+        self.rooms.append(room("Reception",
+                               [item("Screwdriver","sr1"),
+                                item("Flashlight","ft1")],
+                                ["Waiting Room","Storage Closet"],
+                                {"Waiting Room":["Door"],"Storage Closet":["Door"]}))
+        
+        self.rooms.append(room("Toilet",
+                               [item("Toilet Brush","th1"),
+                                item("Bandage","be1"),
+                                item("Toothpaste","te1")],
+                                ["Reception"],
+                                {"Reception":["Door"]}))
+        
+        self.rooms.append(room("Storage Closet",
+                               [item("Mop","mp1"),
+                                item("Drain Unblocker","dr1"),
+                                item("Newspaper","nr1")],
+                                ["Waiting Room"],
+                                {"Waiting Room":["Door"]}))
+        
+        self.rooms.append(room("Hallway",
+                               [item("Sunflower Kirill","sl1")],
+                               [None],
+                               {None:[None]}))
+
+        
+        
         self.current_room = self.rooms[0]
 
 
@@ -25,6 +54,10 @@ class game():
                 self.current_room = room
             else:
                 print("Such a room does not exist. ")
+
+
+
+    def check_availability(self,string):
 
 
         
