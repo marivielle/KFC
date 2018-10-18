372edaa47732f00142b8dcbac4c53b11a13ea76e
EuanMorgan70 <44199299+EuanMorgan70@users.noreply.github.com>
2018-10-18 16:05:22 +0100

    Add files via upload

--- player.py
+++ player.py
@@ -1,17 +1,54 @@
 from inventory import *
-
-class player(room):
+from NPC import *
+import random
+class player():
     
     def __init__(self):
-        self._current_room = None
-        self.inv = inventory.items
-        self.health = 0
         
-    def initialise(self,game_class):
-        self.current_room = game_class.rooms[0].name
+        self.inv = inventory.items
+        self.health = 100
+        self.alive = True
 
-    def change_room(self):
+    def heal(self):
+        
+        if self.health < 100:
+            if "bandage" in self.inv:
+                self.health + random.randrange(8, 36)
+    
+    def print_health(self):
+        
+        print("Player Health: " + self.health)
 
+    
 
+    def attack(self, weapon):
+        weak_weapons = ["id card", "toilet brush", "bandage", "toothpaste", "newspaper"]
+        medium_weapons = ["rock", "mop", "sunflower"]
+        strong_weapons = ["large log", "drain unblocker"]
+        
+        if weapon == "fists":
+            fists_dmg = random.randrange(0.5, 4)
+            
+            print("You hit " + NPC.name + " with " + weapon +
+                  " for " + firsts_dmg + "damage!")
+        
+        elif weapon in weak_weapons:
+            weak_dmg = random.randrange(0, 2)
+            print("You hit " + NPC.name + " with " + weapon +
+                  " for " + weak_dmg + "damage!")
+            return weak_dmg
+            
+        elif weapon in medium_weapons:
+            med_dmg = random.randrange(3, 6)
+            print("You hit " + NPC.name + " with " + weapon +
+                  " for " + med_dmg + "damage!")
+            return med_dmg
 
+        elif weapon in strong_weapons:
+            strong_dmg = random.randrange(7, 10)
+            
+            print("You hit " + NPC.name + " with " + weapon +
+                  " for " + strong_dmg + "damage!")
+            
+            return strong_dmg
         
