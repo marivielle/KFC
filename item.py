489be4cd07a133e8d2eb60f7f906ea60473a6f5d
Mariana Capdevielle Schurmann <marivielle@gmail.com>
2018-10-18 15:50:46 +0100

    Update item.py

--- item.py
+++ item.py
@@ -1,7 +1,6 @@
 class item():
-    def __init__(self, name, description = "", identification, strength = 0):
+    def __init__(self, name, description = "", identification):
         self.name = name
         self.reduce_inv = False #whether the item is large enough to reduce inventory space
         self.description = description #item description
         self.id = identification  #id of item
-        self.item_strength = strength #determines the amount of damage left when hitting with this item
